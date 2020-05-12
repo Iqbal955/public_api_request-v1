85ac98fc1017200473bd81b8a7f78ff67309cf6c
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-05-12 19:55:29 +0200

    p5
    p5

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 //############ DOM elements #############
 const search = document.querySelector("search-container");
 const modal = document.querySelector("div .modal-info-container");
-const modalimg = document.querySelector(".modal-img");
+const modalImg = document.querySelector(".modal-img");
 const modalcontainer = document.getElementById("modal-container");
 
 //############ FETCH ####################
@@ -16,15 +16,17 @@ fetch("https://randomuser.me/api/")
 function showInfo(data) {
   
     
-    console.log(modalimg);
+    
 
     for (var i = 0; i < data.length; i++) {
 
-        modalimg.innerHTML = 
-           
-    ` <img src= ${data[i].picture.large}>`;
+        modalImg.innerHTML = 
+            `
+     <img src= ${data[i].picture.large}>`;
 
-   
+
+        //modalImg.src = `${data[i].picture.large}>`
+    
 
     }
     
