ac84dd0d1621a0883662ac1b09d7611928ebfec6
Iqbal955 <58709666+Iqbal955@users.noreply.github.com>
2020-05-14 17:34:10 +0200

    DOM created for gallery and 12 users

--- js/script.js
+++ js/script.js
@@ -6,32 +6,56 @@ const modalImg = document.querySelector(".modal-img");
 const modalcontainer = document.getElementById("modal-container");
 
 //############ FETCH ####################
-fetch("https://randomuser.me/api/")
-    .then(response => response.json())
-    .then(data => showInfo(data.results))
+
+
+for (var i = 0; i < 12; i++) {
+
+    fetch("https://randomuser.me/api/")
+        .then(response => response.json())
+        .then(data => cards(data.results))
+}
+
+
 
 
 //############ GenerateHTML ##################
 
-function showInfo(data) {
-  
-    
-    
+function cards(data) {
+
 
     for (var i = 0; i < data.length; i++) {
 
-        modalImg.innerHTML = 
-            `
-     <img src= ${data[i].picture.large}>`;
+        console.log(data[i].picture.large);
 
+        const card = document.createElement("div");
+              card.setAttribute("class", "card");
+            document.body.appendChild(card);
+        const cardcontainer = document.createElement("div")
+              cardcontainer.setAttribute("class", "card-img-container");
+              card.appendChild(cardcontainer);
 
-        //modalImg.src = `${data[i].picture.large}>`
-    
+  
+
+        
+        const img = document.createElement("img");
+              img.setAttribute("class", "card-img");
+              img.setAttribute("src", `${data[i].picture.large}`);
+              img.setAttribute("alt", "Profile picture");
+              cardcontainer.appendChild(img);
+
+        
+
+        const cardinfocontainer = document.createElement("div")
+              cardinfocontainer.setAttribute("class", "card-info-container")
+              cardinfocontainer.innerHTML = 
+                  ` <h3 id="name" class= "card-name cap">${data[i].name.first} ${data[i].name.last}</h3>
+                    <p class= "card-text">${data[i].email}</p>
+                    <p class="card-text cap">${data[i].location.city} ${data[i].location.state}</p> `
+              card.appendChild(cardinfocontainer);
+
 
     }
-    
- 
-    }
+}
 
 
 
@@ -39,3 +63,29 @@ function showInfo(data) {
 
 
 
+
+
+
+
+
+
+    /*
+
+<div class="modal-container">
+    <div class="modal">
+        <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+        <div class="modal-info-container">
+            <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
+                <h3 id="name" class="modal-name cap">name</h3>
+                <p class="modal-text">email</p>
+                <p class="modal-text cap">city</p>
+                <hr>
+                    <p class="modal-text">(555) 555-5555</p>
+                    <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                    <p class="modal-text">Birthday: 10/21/2015</p>
+                </div>
+            </div>
+
+*/
+
+  
